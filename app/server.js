b37cd642095e579a6afb7fd685871a25aad43fae
unknown <omotolaadebulu@gmail.com>
2026-04-01 15:42:18 +0100

    updated and optimized server.js

--- app/server.js
+++ app/server.js
@@ -1,84 +1,69 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
-let app = express();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const app = express();
+const PORT = 3000;
+
+// === Body parser middleware ===
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+// === Serve static files from the app folder ===
+app.use(express.static(__dirname));
 
-app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+// === Serve index.html for the root route ===
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+// === MongoDB configuration ===
+// Use environment variable or default to local
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://admin:password@localhost:27017";
 
-// use when starting application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const databaseName = "my-db";
 
-// use when starting application as docker container, part of docker-compose
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+// === Update user profile ===
+app.post('/update-profile', async (req, res) => {
+  const userObj = req.body;
+  try {
+    const client = await MongoClient.connect(mongoUrl, mongoClientOptions);
+    const db = client.db(databaseName);
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
-
-app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
-
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
-
-    let db = client.db(databaseName);
     userObj['userid'] = 1;
+    const myquery = { userid: 1 };
+    const newvalues = { $set: userObj };
 
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
+    await db.collection('users').updateOne(myquery, newvalues, { upsert: true });
+    client.close();
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
-
-  });
-  // Send response
-  res.send(userObj);
+    res.send(userObj);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Database error' });
+  }
 });
 
-app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+// === Get user profile ===
+app.get('/get-profile', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(mongoUrl, mongoClientOptions);
+    const db = client.db(databaseName);
 
-    let db = client.db(databaseName);
+    const result = await db.collection('users').findOne({ userid: 1 });
+    client.close();
 
-    let myquery = { userid: 1 };
-
-    db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
-      client.close();
-
-      // Send response
-      res.send(response ? response : {});
-    });
-  });
+    res.send(result || {});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Database error' });
+  }
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+// === Start server ===
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
 });
-
